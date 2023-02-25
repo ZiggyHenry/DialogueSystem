b59ff8e259d730c815ebc1f510c7d6213f012129
ZigZogger <ziggycooperhenry@gmail.com>
2023-02-25 14:40:45 -0800

    added a lot of functionality to dialogueManager, added gameEvents

--- Dialogue System/Assets/Scripts/DialogueDataBase.cs
+++ Dialogue System/Assets/Scripts/DialogueDataBase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Dialogue System/Dialogue Table")]
-public class DialogueDataBase : ScriptableObject
+public class DialogueDatabase : ScriptableObject
 {
     public DialogueLineData[] data;
 }
---
+++ Dialogue System/Assets/Scripts/GameEvents.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using EvtSystem;
+
+public class ShowDialogueText : EvtSystem.Event
+{
+    public string text;
+    public CharacterID id;
+}
+
+public class PlayAudio : EvtSystem.Event
+{
+    public AudioClip clipToPlay;
+}
+
+public struct responseData
+{
+    public string text;
+    public int karmaScore;
+
+    public UnityAction buttonAction;
+}
+
+public class ShowResponses : EvtSystem.Event
+{
+    public responseData[] responses;
+}
